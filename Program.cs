6af4fe8f6f119dca9be02c5bf352b3f4d3d862da
Sergi-0 <144801688+Sergi-0@users.noreply.github.com>
2024-09-18 12:12:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_1
+{
+    struct Vector
+    {
+        public double x, y, z;
+
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public static Vector operator +(Vector v, Vector t)
+        {
+            return new Vector(v.x + t.x, v.y + t.y, v.z + t.z);
+        }
+
+        public static Vector operator *(Vector v, Vector t)
+        {
+            return new Vector(v.x * t.x, v.y * t.y, v.z * t.z);
+        }
+
+        public static Vector operator *(Vector v, double t)
+        {
+            return new Vector(v.x * t, v.y * t, v.z * t);
+        }
+
+        public static bool operator !=(Vector v, Vector t)
+        {
+            return (Math.Sqrt(v.x*v.x + v.y* v.y + v.z * v.z) != Math.Sqrt(t.x * t.x + t.y * t.y + t.z * t.z));
+        }
+        public static bool operator ==(Vector v, Vector t)
+        {
+            return (Math.Sqrt(v.x * v.x + v.y * v.y + v.z * v.z) == Math.Sqrt(t.x * t.x + t.y * t.y + t.z * t.z));
+        }
+
+        public static bool operator >(Vector v, Vector t)
+        {
+            return (Math.Sqrt(v.x * v.x + v.y * v.y + v.z * v.z) > Math.Sqrt(t.x * t.x + t.y * t.y + t.z * t.z));
+        }
+
+        public static bool operator <(Vector v, Vector t)
+        {
+            return (Math.Sqrt(v.x * v.x + v.y * v.y + v.z * v.z) < Math.Sqrt(t.x * t.x + t.y * t.y + t.z * t.z));
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(2,3,4);
+            Vector v2 = new Vector(3,4,5);
+            Vector v3 = v1 + v2;
+            Vector v4 = v1 * v2;
+            Vector v5 = v1 * 2;
+            Vector v6 = new Vector(3,4,5);
+
+            Console.WriteLine(v6 == v1);
+            Console.WriteLine(v6 == v2);
+            Console.WriteLine(v2 > v1);
+            Console.WriteLine(v2 < v1);
+            Console.WriteLine(v2 != v1);
+            Console.WriteLine($"{v3.x} {v3.y} {v3.z}");
+        }
+    }
+}
